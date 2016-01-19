fd04ff3905c8b7f647536e91dd41171557924952
János Illés <ijanos@gmail.com>
2016-01-19 16:51:52 +0100

    project euler problem 2

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,34 @@ fn problem1c() {
    );
 }
 
+struct Fib {
+   curr: u32,
+   next: u32
+}
+
+impl Iterator for Fib {
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        let new = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new;
+
+        Some(self.curr)
+    }
+}
+
+fn euler2() {
+    let fib = Fib {curr: 1, next: 1};
+    println!("{}", fib
+            .take_while(|&a| a < 4_000_000)
+            .filter(|&a| a % 2 == 0)
+            .fold(0, |sum, i| sum + i));
+}
+
 fn main() {
     problem1a();
     problem1b();
     problem1c();
+    euler2();
 }
