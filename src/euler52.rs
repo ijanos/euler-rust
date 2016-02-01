788b280a5944258f0b4730b7265fc34764662685
János Illés <ijanos@gmail.com>
2016-02-01 23:26:13 +0100

    problem 52: reorder list of multiples
    all will short circuit faster this way, makes the computation faster.

--- src/euler52.rs
+++ src/euler52.rs
@@ -5,13 +5,13 @@ fn same_digits(a: u64, b: u64) -> bool {
     let bstr = b.to_string();
 
     if astr.len() != bstr.len() { return false }
-  
+
     let aset : HashSet<char> = astr.chars().collect();
     let bset : HashSet<char> = bstr.chars().collect();
-  
+
     aset == bset
  }
 
  pub fn main() {
-    println!("{}", (1..).filter(|&n| [2*n,3*n,4*n,5*n,6*n].iter().all(|&m| same_digits(n,m))).nth(0).unwrap());
+    println!("{}", (1..).filter(|&n| [6*n, 5*n, 4*n, 3*n, 2*n].iter().all(|&m| same_digits(n,m))).nth(0).unwrap());
  }
