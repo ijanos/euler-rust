4f716563f04c4c97399c3339e3924ce1bcf4084a
János Illés <ijanos@gmail.com>
2016-02-02 19:03:51 +0100

    clean up problem 92

--- src/euler92.rs
+++ src/euler92.rs
@@ -1,11 +1,10 @@
 fn chain89(start: u32) -> bool {
-    let mut digits: Vec<u32> = start.to_string().chars().map(|c| c.to_digit(10).unwrap()).collect();
-    let mut sum;
+    let mut sum = start;
     loop {
+        let digits: Vec<u32> = sum.to_string().chars().map(|c| c.to_digit(10).unwrap()).collect();
         sum = digits.iter().fold(0, |acc, n| acc + n*n);
         if sum == 1 { return false; }
         if sum == 89 { return true; }
-        digits = sum.to_string().chars().map(|c| c.to_digit(10).unwrap()).collect();
     }
 }
 
