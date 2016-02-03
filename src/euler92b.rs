40e32487d567a296183cffd9caa81d215343baf3
János Illés <ijanos@gmail.com>
2016-02-03 21:32:25 +0100

    problem 92b: allocate the array on the heap

--- src/euler92b.rs
+++ src/euler92b.rs
@@ -1,6 +1,6 @@
 const MAX: usize = 10_000_000;
 
-fn fill(array: &mut [u8; MAX], index: usize) -> u8 {
+fn fill(array: &mut Box<[u8; MAX]>, index: usize) -> u8 {
     let digits: Vec<u32> = index.to_string().chars().map(|c| c.to_digit(10).unwrap()).collect();
     let next = digits.iter().fold(0, |acc, n| acc + n*n) as usize;
     if array[next] == 0 {
@@ -10,7 +10,7 @@ fn fill(array: &mut [u8; MAX], index: usize) -> u8 {
 }
 
 pub fn main() {
-    let mut numbers: [u8; MAX] = [0; MAX];
+    let mut numbers: Box<[u8; MAX]> = Box::new([0; MAX]);
     numbers[1] = 1;
     numbers[89] = 89;
     for i in 2..MAX {
