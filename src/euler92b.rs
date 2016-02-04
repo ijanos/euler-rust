cef63ac0376217a4958e96ab6035bd1b4faecbd4
János Illés <ijanos@gmail.com>
2016-02-04 19:37:13 +0100

    make problem 92b faster
    - use vector instead of an array for heap allocation
    - merge two iterators into one

--- src/euler92b.rs
+++ src/euler92b.rs
@@ -1,8 +1,14 @@
 const MAX: usize = 10_000_000;
 
-fn fill(array: &mut Box<[u8; MAX]>, index: usize) -> u8 {
-    let digits: Vec<u32> = index.to_string().chars().map(|c| c.to_digit(10).unwrap()).collect();
-    let next = digits.iter().fold(0, |acc, n| acc + n*n) as usize;
+fn fill(array: &mut Vec<u8>, index: usize) -> u8 {
+    let next = index
+                .to_string()
+                .chars()
+                .map(|c| {
+                    let n = c.to_digit(10).unwrap();
+                    n*n
+                 })
+                 .fold(0, |acc, n| acc + n) as usize;
     if array[next] == 0 {
         array[next] = fill(array, next);
     }
@@ -10,7 +16,7 @@ fn fill(array: &mut Box<[u8; MAX]>, index: usize) -> u8 {
 }
 
 pub fn main() {
-    let mut numbers: Box<[u8; MAX]> = Box::new([0; MAX]);
+    let mut numbers = vec![0; MAX];
     numbers[1] = 1;
     numbers[89] = 89;
     for i in 2..MAX {
