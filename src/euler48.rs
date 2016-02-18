ce2b31c85bea4681e67305ab4a21bb7267e4cd71
János Illés <ijanos@gmail.com>
2016-02-18 07:40:43 +0100

    solution for problem 48

---
+++ src/euler48.rs
@@ -0,0 +1,9 @@
+use num::bigint::ToBigInt;
+use num;
+
+pub fn main() {
+    let ans: num::BigInt = (1..1001)
+                            .map(|x| num::pow(x.to_bigint().unwrap(), x))
+                            .fold(num::zero(), |acc, x| acc + x);
+    println!("{}", ans.to_string().chars().rev().take(10).collect::<Vec<_>>().into_iter().rev().collect::<String>());
+}
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ mod euler14;
 mod euler15;
 mod euler20;
 mod euler22;
+mod euler48;
 mod euler49;
 mod euler50;
 mod euler52;
@@ -44,6 +45,7 @@ fn main() {
             "14" => euler14::main(),
             "15" => euler15::main(),
             "20" => euler20::main(),
+            "48" => euler48::main(),
             "49" => euler49::main(),
             "50" => euler50::main(),
             "22" => euler22::main(),
