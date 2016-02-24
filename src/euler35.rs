4e88af56a03485dd7fc4fe7aa2f0e932c8d6c207
János Illés <ijanos@gmail.com>
2016-02-24 07:43:21 +0100

    make solution 35 faster

--- src/euler35.rs
+++ src/euler35.rs
@@ -42,8 +42,12 @@ impl<T: Clone> Iterator for Rotator<T> {
 pub fn main() {
     let primes = (2 as u64..).filter(|&n| (2..(n as f32).sqrt() as u32 + 1).all(|i| n % i as u64 != 0));
     let primes_below1m = primes.take_while(|&n| n <  1_000_000).collect::<Vec<_>>();
+    let banned = ['0', '2', '4', '5', '6', '8'];
     let validrotates = |n: &u64| {
         let chars = n.to_string().chars().collect::<Vec<_>>();
+        if chars.len() > 1 && banned.iter().any(|n| chars.contains(n)) {
+            return false;
+        }
         let rotations = Rotator::new(chars);
         rotations.map(|r| array2num(r.iter().map(|&c| c.to_digit(10).unwrap()).collect::<Vec<u32>>()))
                  .all(|p| primes_below1m.contains(&p))
