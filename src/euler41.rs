23882f182a7f44754724135e2c1c4f2ea1730afa
János Illés <ijanos@gmail.com>
2016-02-25 08:12:04 +0100

    problem 41

---
+++ src/euler41.rs
@@ -0,0 +1,16 @@
+use std::char;
+
+pub fn main() {
+    let primes = (2..).filter(|&n| (2..(n as f32).sqrt() as u32 + 1).all(|i| n % i != 0));
+    let mut max = 0;
+    for p in primes.take_while(|&p| p <= 7654321) {
+        let s = p.to_string();
+        if (1..s.len()+1).all(|n|
+                s.chars().any(|p| p == char::from_digit(n as u32, 10).unwrap())) {
+            if p > max {
+                max = p;
+            }
+        }
+    }
+    println!("{}", max);
+}
