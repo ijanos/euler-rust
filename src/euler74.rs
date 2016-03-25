ebf49a73435ed8eb4cc473bcc3781774d6a61d4b
János Illés <ijanos@gmail.com>
2016-03-25 07:20:33 +0100

    switch to common function

--- src/euler74.rs
+++ src/euler74.rs
@@ -1,29 +1,10 @@
-fn factsum(n: usize) -> usize {
-    n.to_string()
-     .chars()
-     .map(|c| {
-         match c {
-             '0' => 1,
-             '1' => 1,
-             '2' => 2,
-             '3' => 6,
-             '4' => 24,
-             '5' => 120,
-             '6' => 720,
-             '7' => 5040,
-             '8' => 40320,
-             '9' => 362880,
-             _ => panic!(),
-         }
-     })
-     .fold(0, |x, acc| acc + x)
-}
+use common::digit_fact_sum;
 
 fn fill(cache: &mut Vec<usize>, i: usize) -> usize {
     let mut chain = vec![i];
     let mut next = i;
     loop {
-        next = factsum(next);
+        next = digit_fact_sum(next);
         if chain.contains(&next) {
             return chain.len();
         } else if cache.len() > next && cache[next] != 0 {
