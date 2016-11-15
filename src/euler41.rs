64e830a51ebc5da9eda9ec618b0918c3ec208d34
János Illés <ijanos@gmail.com>
2016-11-15 13:03:25 +0100

    euler 41: solve without mutable variables

--- src/euler41.rs
+++ src/euler41.rs
@@ -2,15 +2,10 @@ use std::char;
 
 pub fn main() {
     let primes = (2..).filter(|&n| (2..(n as f32).sqrt() as u32 + 1).all(|i| n % i != 0));
-    let mut max = 0;
-    for p in primes.take_while(|&p| p <= 7654321) {
-        let s = p.to_string();
-        if (1..s.len() + 1)
-               .all(|n| s.chars().any(|p| p == char::from_digit(n as u32, 10).unwrap())) {
-            if p > max {
-                max = p;
-            }
-        }
-    }
-    println!("{}", max);
+    let pandigital = |number: &u32| {
+        let s = number.to_string();
+        (1..s.len() + 1).all(|n| s.chars().any(|p| p == char::from_digit(n as u32, 10).unwrap()))
+    };
+    let ans = primes.take_while(|&p| p <= 7654321).filter(pandigital).last().unwrap();
+    println!("{}", ans);
 }
